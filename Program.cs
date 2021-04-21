53765224274a712742165b3d1c86b59b889ba00c
jedrzwi1 <wiktoria.jedrzejewska@posti.com>
2021-04-21 12:25:13 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace jenkins_training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
